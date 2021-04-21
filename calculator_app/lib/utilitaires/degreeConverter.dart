34498eb3b5314f1b930e48859e8e75dec004d687
maxime Pajard <m.pajard@gmail.com>
2021-04-21 17:55:00 +0200

    Température début

--- calculator_app/lib/utilitaires/degreeConverter.dart
+++ calculator_app/lib/utilitaires/degreeConverter.dart
@@ -12,6 +12,45 @@ class DegreeConverterPage extends StatefulWidget  {
 
 class _DegreeConverterPageState extends State<DegreeConverterPage> {
 
+  String dropdownValue = 'Celsius';
+  String dropdownValue2 = 'Kelvin';
+  var myController1 = TextEditingController();
+  var myController2 = TextEditingController();
+  num dValue = 0;
+  num cValue = 0;
+  num kValue = 0;
+  num fValue = 0;
+
+
+
+//   void stringToDouble(tValue) {
+//     dValue = double.parse(tValue);
+//  }
+//
+//
+//   resultatDansTextfield2(value){
+//     myController2.text = '$kValue' ;
+// }
+ celsiusToKelvin(String tValue){
+    dValue = num.parse('$tValue');
+    kValue = dValue + 273.15;
+    myController2.text = kValue.toStringAsFixed(2);
+  }
+
+
+
+
+
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController1.dispose();
+    myController2.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +58,84 @@ class _DegreeConverterPageState extends State<DegreeConverterPage> {
         title: Text(widget.title),
       ),
       body: Center(
-          child: Text("// TODO: Ajouter l'utilitaire qui permet de convertir les températures (cf: Story 9)")
+        child: Column(
+          children : <Widget>[
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              onChanged: (String? newValue){
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: <String>['Celsius', 'Kelvin', 'Farenheit']
+                .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              })
+              .toList(),
+
+            ),
+            TextField(
+              controller: myController1,
+            //  onChanged: celsiusToKelvin(myController1.text),
+
+
+
+
+
+
+            ),
+            DropdownButton<String>(
+              value: dropdownValue2,
+              icon: Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              onChanged: (String? newValue2){
+                setState(() {
+                  dropdownValue2 = newValue2!;
+                });
+              },
+              items: <String>['Celsius', 'Kelvin', 'Farenheit']
+                  .map<DropdownMenuItem<String>>((String value2) {
+                return DropdownMenuItem<String>(
+                  value: value2,
+                  child: Text(value2),
+                );
+              })
+                  .toList(),
+
+            ),
+
+            TextField(
+              controller: myController2,
+          //    onChanged: celsiusToKelvin(myController2.text),
+
+
+
+
+
+
+            ),
+
+
+
+
+            // ElevatedButton(
+            //   child: Text('convert'),
+            //   onPressed: () => {
+            //     celsiusToKelvin(myController1.text),
+            //
+            //
+            //   },
+            //
+            // ),
+          ],
+        ),
       ),
     );
   }
